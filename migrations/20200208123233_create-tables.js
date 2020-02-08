8374f8e49b288c080ffc31b30270a0fe077dbb74
Katrina_Dierking <kalynndier@gmail.com>
2020-02-08 12:53:52 -0500

    resources and tasks tables created

--- migrations/20200208123233_create-tables.js
+++ migrations/20200208123233_create-tables.js
@@ -3,16 +3,29 @@ exports.up = async (knex) => {
     await knex.schema.createTable('projects', (table) => {
         table.increments('id').unique().notNullable();
         table.text('projects_name').uniqute().notNullable();
-        table.string('projects_description');
-        table.boolean('completed').notNullable().defaultTo(false);
+        table.string('projects_description', 159);
+        table.boolean('projects_completed').notNullable().defaultTo(false);
     })
 
-    await knex.schema.createTable('presources', table => {
-        
+    await knex.schema.createTable('resources', table => {
+        table.increments('id').unique().notNullable();
+        table.text('resources_name').notNullable();
+        table.string('resources_description', 159);
+
     })
 
     await knex.schema.createTable('tasks', table => {
-        
+        table.increments('id').unique().notNullable();
+        table.string('tasks_description', 159).notNullable();
+        table.text('tasks_notes');
+        table.boolean('tasks_completed').notNullable().defaultTo(false);
+        table.integer('projects_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .ondDelete('CASCADE');
     })
   
 };
